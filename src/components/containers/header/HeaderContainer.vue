2aa5e36321c18a0dc80c5f3cd5a6a033c689b7c0
qiushiming <qiushiming1997@gmail.com>
2023-06-06 09:08:22 +0800

    first commit

---
+++ src/components/containers/header/HeaderContainer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <div>
+    <button @click="toggleDark()">toggleDark</button>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import HeaderContainer from '@/components/containers/header/HeaderContainer.vue'
+import TrackContainer from '@/components/containers/track/TrackContainer.vue';
+import PlayerContainer from '@/components/containers/player/PlayerContainer.vue';
+import ResourceContainer from '@/components/containers/resource/ResourceContainer.vue'
+import AttributeContainer from '@/components/containers/attribute/AttributeContainer.vue'
+
+</script>
+
+<template>
+  <HeaderContainer />
+  <main class="home-main">
+    <ResourceContainer />
+    <div class="editor-wrapper">
+      <div class="player-container">
+        <PlayerContainer />
+        <AttributeContainer />
+      </div>
+      <TrackContainer />
+    </div>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.home-main {
+  display: flex;
+  background: var(--app-background-color);
+
+  .editor-wrapper {
+    flex-shrink: 0;
+    color: black;
+
+    .player-container {
+      display: flex;
+      justify-content: flex-start;
+    }
+  }
+}
+</style>
