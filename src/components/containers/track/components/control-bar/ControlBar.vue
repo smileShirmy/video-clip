5bba71c8f3eaf323d36c6f79c82e3e11527eaacd
qiushiming <qiushiming1997@gmail.com>
2023-06-13 17:53:01 +0800

    feat(scale-slider): add scale-slider component

--- src/components/containers/track/components/control-bar/ControlBar.vue
+++ src/components/containers/track/components/control-bar/ControlBar.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ScaleSlider from './ScaleSlider.vue'
+</script>
 
 <template>
-  <div class="control-bar">ControlBar</div>
+  <div class="control-bar"><ScaleSlider /></div>
 </template>
 
 <style scoped lang="scss">
 .control-bar {
+  display: flex;
+  align-items: center;
   box-sizing: border-box;
   height: 30px;
   background-color: var(--app-bg-color-light);
---
+++ src/components/containers/track/components/control-bar/ScaleSlider.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+function onSliderDown() {
+  //
+}
+</script>
+
+<template>
+  <div class="scale-slider">
+    <div class="slider-runway" @mousedown="onSliderDown" @touchstart="onSliderDown">
+      <div class="slider-bar"></div>
+      <div class="slider-button"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.scale-slider {
+  display: flex;
+  align-items: center;
+  width: 100px;
+  height: 12px;
+
+  .slider-runway {
+    position: relative;
+    flex: 1;
+    cursor: pointer;
+    height: 2px;
+    background-color: var(--app-bg-color-lighter);
+
+    .slider-bar {
+      width: 7%;
+      height: 100%;
+      left: 0%;
+      background-color: var(--app-color-white);
+    }
+
+    .slider-button {
+      position: absolute;
+      top: -5px;
+      left: 7%;
+      cursor: grab;
+      height: 12px;
+      width: 6px;
+      border-radius: 3px;
+      background-color: var(--app-color-white);
+      transform: translate(-50%);
+      user-select: none;
+    }
+  }
+}
+</style>
