637e9fd2d30033cc04b509af9e2c2192e00e7f34
qiushiming <qiushiming1997@gmail.com>
2023-07-28 17:51:18 +0800

    chore: add moveable player

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MoveablePlayer from '../views/MoveablePlayer.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/moveable-player',
+      name: 'moveable-player',
+      component: MoveablePlayer
     }
   ]
 })
