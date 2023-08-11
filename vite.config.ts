d30190929d8529ef90bce69bb408a24ead758074
qiushiming <qiushiming1997@gmail.com>
2023-08-11 10:27:56 +0800

    chore: 增加 dev-ssl 命令用 https 访问以解决域名访问无法加载 ffmpeg 的问题

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { mockServer } from './plugins/vite-plugin-mock-server'
+const fs = require('fs')
+
+// 通过 dev-ssl 命令开启 https 以支持通过域名访问
+const openSSL = process.env?.npm_lifecycle_event === 'dev-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), mockServer()],
   server: {
+    host: openSSL,
+    https: openSSL && {
+      key: fs.readFileSync('plugins/cert/key.pem'),
+      cert: fs.readFileSync('plugins/cert/cert.pem')
+    },
     // 支持 wasm 数据隔离
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
