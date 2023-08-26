79d4e9ead4e466a3559680360d1f55d62d52f9ff
qiushiming <qiushiming1997@gmail.com>
2023-08-26 15:36:28 +0800

    feat: 视频类型和贴图类型撤回回退

--- src/components/containers/attribute/video-attribute/video-picture/VideoPicture.vue
+++ src/components/containers/attribute/video-attribute/video-picture/VideoPicture.vue
@@ -5,88 +5,139 @@ import InputNumber from '../../components/base/InputNumber.vue'
 import AttributeItem from '../../components/layout/AttributeItem.vue'
 import { computed } from 'vue'
 import type { VideoTrackItem } from '@/services/track-item/video-track-item'
+import { PlayerAttributeChangeAction } from '@/services/steps-manager/player-attribute-action'
 
 defineOptions({
   name: 'VideoPicture'
 })
 
 const trackStore = useTrackStore()
-const trackItem: VideoTrackItem = trackStore.selectedTrackItem as VideoTrackItem
+const trackItem = computed(() => trackStore.selectedTrackItem as VideoTrackItem)
 
 const opacity = computed({
   get() {
-    return trackItem.attribute.opacity * 100
+    return trackItem.value.attribute.opacity * 100
   },
   set(opacity) {
-    trackItem.attribute.opacity = opacity / 100
+    trackItem.value.attribute.opacity = opacity / 100
   }
 })
+
+function onOpacityChange(newVal: number, oldVal: number) {
+  new PlayerAttributeChangeAction(trackItem.value.id, {
+    opacity: oldVal / 100
+  }).end({
+    opacity: newVal / 100
+  })
+}
 
 const scale = computed({
   get() {
-    return trackItem.attribute.scale * 100
+    return trackItem.value.attribute.scale * 100
   },
   set(scale) {
-    trackItem.attribute.scale = scale / 100
+    trackItem.value.attribute.scale = scale / 100
   }
 })
+
+function onScaleChange(newVal: number, oldVal: number) {
+  new PlayerAttributeChangeAction(trackItem.value.id, {
+    scale: oldVal / 100
+  }).end({
+    scale: newVal / 100
+  })
+}
 
 const rotate = computed({
   get() {
-    return trackItem.attribute.rotate
+    return trackItem.value.attribute.rotate
   },
   set(rotate) {
-    trackItem.attribute.rotate = rotate
+    trackItem.value.attribute.rotate = rotate
   }
 })
+
+function onRotateChange(newVal: number, oldVal: number) {
+  new PlayerAttributeChangeAction(trackItem.value.id, {
+    rotate: oldVal
+  }).end({
+    rotate: newVal
+  })
+}
 
 const translateX = computed({
   get() {
-    return trackItem.attribute.leftRatio * 100
+    return trackItem.value.attribute.leftRatio * 100
   },
   set(x) {
-    trackItem.attribute.leftRatio = x / 100
+    trackItem.value.attribute.leftRatio = x / 100
   }
 })
 
+function onTranslateXChange(newVal: number, oldVal: number) {
+  new PlayerAttributeChangeAction(trackItem.value.id, {
+    leftRatio: oldVal / 100
+  }).end({
+    leftRatio: newVal / 100
+  })
+}
+
 const translateY = computed({
   get() {
-    return trackItem.attribute.topRatio * 100
+    return trackItem.value.attribute.topRatio * 100
   },
   set(y) {
-    trackItem.attribute.topRatio = y / 100
+    trackItem.value.attribute.topRatio = y / 100
   }
 })
+
+function onTranslateYChange(newVal: number, oldVal: number) {
+  new PlayerAttributeChangeAction(trackItem.value.id, {
+    topRatio: oldVal / 100
+  }).end({
+    topRatio: newVal / 100
+  })
+}
 </script>
 
 <template>
   <AttributeItem label="不透明度">
-    <AttributeSlider v-model="opacity" />
+    <AttributeSlider v-model="opacity" @change="onOpacityChange" />
     <template #other>
-      <InputNumber v-model="opacity" unit="%" />
+      <InputNumber v-model="opacity" unit="%" @change="onOpacityChange" />
     </template>
   </AttributeItem>
   <AttributeItem label="缩放">
-    <AttributeSlider :max="500" v-model="scale" />
+    <AttributeSlider :max="500" v-model="scale" @change="onScaleChange" />
     <template #other>
-      <InputNumber :max="500" v-model="scale" unit="%" />
+      <InputNumber :max="500" v-model="scale" unit="%" @change="onScaleChange" />
     </template>
   </AttributeItem>
   <AttributeItem label="坐标">
     <div class="s-coordinate">
-      <InputNumber :min="-Infinity" :max="Infinity" v-model="translateX">
+      <InputNumber
+        :min="-Infinity"
+        :max="Infinity"
+        v-model="translateX"
+        @change="onTranslateXChange"
+      >
         <template #prefix>X</template>
       </InputNumber>
       <span class="s-attribute-item-line"></span>
-      <InputNumber :min="-Infinity" :max="Infinity" v-model="translateY">
+      <InputNumber
+        :min="-Infinity"
+        :max="Infinity"
+        v-model="translateY"
+        @change="onTranslateYChange"
+      >
         <template #prefix>Y</template>
       </InputNumber>
     </div>
   </AttributeItem>
   <AttributeItem label="旋转">
-    <AttributeSlider :max="360" v-model="rotate" />
+    <AttributeSlider :max="360" v-model="rotate" @change="onRotateChange" />
     <template #other>
-      <InputNumber :max="360" v-model="rotate" unit="°" />
+      <InputNumber :max="360" v-model="rotate" unit="°" @change="onRotateChange" />
     </template>
   </AttributeItem>
 </template>
